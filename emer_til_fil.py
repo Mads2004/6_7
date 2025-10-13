7fbe190fbd7578b9b06a411480100f8632dd241e
olli061201-source <olli06.12.01@gmail.com>
2025-10-13 17:36:09 +0200

    Update emer_til_fil.py

--- emer_til_fil.py
+++ emer_til_fil.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  9 21:01:20 2025
+from itertools import zip_longest
 
-@author: Olli
-"""
-
-emner=["matte", "norsk", "engelsk", "naturfag", "historie", "gym"]
 
 try:
     with open ("studiestuff.txt", "w") as fil:
+        fil.write(f"{'Studieplan':<10} | {'Emner':>10}\n")
+        fil.write("" "\n")
         
-        
-        fil.write("Emner\n")
-        for emne in emner:
-            fil.write(f"- {emne}\n")
+        for emne, studie in zip_longest(emnekode, semester, fillvalue="."):
+           fil.write(f"{studie:<10} | {emne:>10}\n")
+
 
 except FileNotFoundError:
     print("fant ikke flien")
+
+
