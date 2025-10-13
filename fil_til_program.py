9c12de179cf768000659a8adff15f2901d2cad22
olli061201-source <olli06.12.01@gmail.com>
2025-10-13 15:58:48 +0200

    Update fil_til_program.py

--- fil_til_program.py
+++ fil_til_program.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  9 21:39:20 2025
-
-@author: Olli
-"""
-studieplan= list()
-emner= list()
-
 try:
-    with open ("studiestuff.txt", "r",encoding=UTTF-8) as fil:
-        fil.readline()
-        for linje in fil:
-            linje = linje.strip()
-            
+    with open ("studiestuff.txt", "r") as fil:
+        skriv_ut= fil.read()
+        print(skriv_ut)
+except FileNotFoundError:
+    print("filen finnes ikke")
