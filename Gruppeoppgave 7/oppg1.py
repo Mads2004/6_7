f974554e484e98bc688663530c5343b3d93c3b55
Jawosi <sijac2582@uis.no>
2025-10-14 17:54:47 +0200

    Ferdig
    Ferdig og fungerende dokument, kan evt. legge inn except på alle nødvendige hvis tid

--- Gruppeoppgave 7/oppg1.py
+++ Gruppeoppgave 7/oppg1.py
@@ -2,28 +2,43 @@
 """
 Created on Mon Oct 13 17:22:09 2025
 
+<<<<<<< Updated upstream
 @author: simen
+=======
+@author: Mads
+>>>>>>> Stashed changes
 """
 emnekode = []
 semester = []
 studiepoeng = []
 
 def oppgave1(emnekode, semester, studiepoeng):
+<<<<<<< Updated upstream
     #emnekode = []
     #semester = []
     #studiepoeng = []
     
     print("=== Registrering av emner ===")
+=======
+    
+    print("Registrering av emner")
+>>>>>>> Stashed changes
     print("Skriv 'stopp' som emnekode for å avslutte.\n")
     
     while True:
         kode = input("Skriv inn emnekode: ").upper()
     
+<<<<<<< Updated upstream
         # Avslutt hvis brukeren skriver 'stopp'
         if kode == "STOPP":
             break
     
         # Sjekk om emnet finnes fra før
+=======
+        if kode == "STOPP":
+            break
+    
+>>>>>>> Stashed changes
         if kode in emnekode:
             print(" Dette emnet finnes allerede. Prøv igjen.\n")
             continue
@@ -31,17 +46,27 @@ def oppgave1(emnekode, semester, studiepoeng):
         sem = input("Hvilket semester undervises emnet (høst/vår): ").lower()
         poeng = int(input("Antall studiepoeng: "))
     
+<<<<<<< Updated upstream
         # Legg til i listene
+=======
+>>>>>>> Stashed changes
         emnekode.append(kode)
         semester.append(sem)
         studiepoeng.append(poeng)
     
         print(f" Emnet {kode} ({poeng} stp, {sem}) er lagt til.\n")
     
+<<<<<<< Updated upstream
     # Etter brukeren avslutter, vis alle emner
+=======
+>>>>>>> Stashed changes
     print("\n--- Registrerte emner ---")
     for i in range(len(emnekode)):
         print(f"{emnekode[i]} - {semester[i]} - {studiepoeng[i]} stp")
     
+<<<<<<< Updated upstream
     print("\nProgrammet er avsluttet.")
+=======
+    print("\nSender bruker tilbake til meny.")
+>>>>>>> Stashed changes
     return emnekode, semester, studiepoeng
--- Gruppeoppgave 7/oppg5.py
+++ Gruppeoppgave 7/oppg5.py
@@ -2,6 +2,7 @@
 """
 Created on Mon Oct 13 15:55:17 2025
 
+<<<<<<< Updated upstream
 @author: simen
 """
 
@@ -20,12 +21,32 @@ def oppgave5(emnekode, semester, studiepoeng):
         total = 0
         for idx in studieplan[semnr]:
             total += studiepoeng[idx]
+=======
+@author: Jan Ståle
+"""
+  
+def oppgave5(studieplan, studiepoeng, emnekode):
+    gyldig = True
+    avvik = []
+
+    for semnr in range(1, 7):
+        total = 0
+        for emne in studieplan[semnr - 1]:
+            if emne in emnekode:
+                index = emnekode.index(emne)
+                total += studiepoeng[index]
+            else:
+                print(f"Advarsel: Emnekode '{emne}' finnes ikke i listen over registrerte emner.")
+>>>>>>> Stashed changes
 
         if total != 30:
             gyldig = False
             avvik.append((semnr, total))
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     if gyldig:
         print("Studieplanen er gyldig (alle semestre har 30 studiepoeng).")
     else:
@@ -33,5 +54,10 @@ def oppgave5(emnekode, semester, studiepoeng):
         for semnr, sp in avvik:
             sesong = "Høst" if semnr in (1, 3, 5) else "Vår"
             print(f"  - Semester {semnr} ({sesong}): {sp} studiepoeng")
+<<<<<<< Updated upstream
 
     return gyldig, avvik
+=======
+            
+    return gyldig, avvik
+>>>>>>> Stashed changes
